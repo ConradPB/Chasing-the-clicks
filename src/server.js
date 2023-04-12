8005ce220174b35231cb3d1d32c0336931c9df7f
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-04-12 07:20:36 +0300

    project setup complete: added cors middleware and a GET request

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
-import 'dotenv/config';
-import express from 'express';
+import 'dotenv/config'
+import cors from 'cors'
+import express from 'express'
 
-const app = express();
+const app = express()
+
+app.use(cors());
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
 
 app.listen(3000, () =>
-  console.log('Example app listening on port 3000!'),
+  console.log(`App is running on ${process.env.PORT} !`),
 );
